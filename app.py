a50bf451664b798d6846136019144232fd95f953
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-08-23 16:31:06 -0700

    completed filling out results.html and result decorator

--- app.py
+++ app.py
@@ -10,7 +10,20 @@ debug = DebugToolbarExtension(app)
 
 @app.get('/')
 def build_form():
+    """ Uses input story prompt to render our form """
     required_words = silly_story.prompts
 
     return render_template("questions.html",
                            word_fields=required_words)
+
+@app.get("/results")
+def post_results():
+    """ Calling get_result_text of story instance to create our story and
+        rendering it to the results html """
+
+    story = silly_story.get_result_text(request.args)
+
+    return render_template(
+        "results.html",
+        story_text = story
+    )
